8c08900de1e0abdfd55570291ab7ceeeb39783a4
eora21 <eora21@naver.com>
2022-12-12 17:50:23 +0900

    feat: 🎸 추가 출력을 위한 get 작성 및 출력문 조사 수정

--- src/main/java/domain/PaymentTool.java
+++ src/main/java/domain/PaymentTool.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 import java.util.function.IntUnaryOperator;
 
 public enum PaymentTool {
-    CARD(1, "신용 카드", total -> total),
-    CASH(2, "현금", total -> (int)(total * 0.95));
+    CARD(1, "신용 카드는", total -> total),
+    CASH(2, "현금은", total -> (int)(total * 0.95));
 
     private final int number;
     private final String description;
@@ -22,5 +22,13 @@ public enum PaymentTool {
                 .filter(paymentTool -> paymentTool.number == number)
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("입력값을 다시 확인해주세요"));
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDescription() {
+        return description;
     }
 }
