6d2bf117f58181d90cb3eec7c1844e586617fd36
eora21 <eora21@naver.com>
2022-12-12 17:31:06 +0900

    feat: 🎸 추가 출력을 위한 number 반환 메서드

--- src/main/java/domain/Table.java
+++ src/main/java/domain/Table.java
@@ -11,6 +11,10 @@ public class Table {
         return tableNumber == number;
     }
 
+    public int getNumber() {
+        return number;
+    }
+
     @Override
     public String toString() {
         return Integer.toString(number);
