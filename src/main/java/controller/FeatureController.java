34a67d4cb2bebad00c4becacc99ff75d26ccb8a5
eora21 <eora21@naver.com>
2022-12-12 16:14:42 +0900

    feat: 🎸 추가 주문 컨트롤러 동작

--- src/main/java/controller/FeatureController.java
+++ src/main/java/controller/FeatureController.java
@@ -1,19 +1,39 @@
 package controller;
 
 import domain.Feature;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
 import view.InputView;
+import view.OutputView;
 
 public class FeatureController {
 
-    public void selectFeature() {
+    private final OrderController orderController = new OrderController();
+
+    public boolean run() {
+        try {
+            return selectFeature();
+        } catch (IllegalArgumentException e) {
+            OutputView.printError(e.getMessage());
+            return false;
+        }
+    }
+
+    private boolean selectFeature() {
         int featureNumber = InputView.inputFeature();
         Feature feature = Feature.findFeatureByNumber(featureNumber);
+
+        if (feature == Feature.QUIT) {
+            return true;
+        }
+
         activeFeature(feature);
+        return false;
     }
 
     private void activeFeature(Feature feature) {
         if (feature == Feature.ORDER) {
-            // 주문 컨트롤러 동작
+            orderController.addOrder();
             return;
         }
 
@@ -21,8 +41,5 @@ public class FeatureController {
             // 결제 컨트롤러 동작
             return;
         }
-
-        //프로그램 종료
-        return;
     }
 }
