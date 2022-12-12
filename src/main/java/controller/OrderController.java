d051edeb7a103d241e54342a06e307ebcd62aac4
eora21 <eora21@naver.com>
2022-12-12 15:10:15 +0900

    feat: 🎸 추가 주문을 받는 메서드

--- src/main/java/controller/OrderController.java
+++ src/main/java/controller/OrderController.java
@@ -3,6 +3,7 @@ package controller;
 import domain.Menu;
 import domain.Order;
 import domain.Table;
+import repository.MenuRepository;
 import repository.TableRepository;
 import view.InputView;
 import view.OutputView;
@@ -10,13 +11,22 @@ import view.OutputView;
 public class OrderController {
     private final Order order = new Order();
 
-    public void selectOrder() {
+    public void addOrder() {
         Table table = selectTable();
+        Menu menu = selectMenu();
+        int orderCount = InputView.inputOrderCount();
+        order.add(table, menu, orderCount);
     }
 
     private Table selectTable() {
         OutputView.printTables(TableRepository.tables());
         int tableNumber = InputView.inputTableNumber();
         return TableRepository.selectTable(tableNumber);
+    }
+
+    private Menu selectMenu() {
+        OutputView.printMenus(MenuRepository.menus());
+        int menuNumber = InputView.inputMenuNumber();
+        return MenuRepository.selectMenu(menuNumber);
     }
 }
