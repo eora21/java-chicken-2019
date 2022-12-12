afe91f5e598fb0ef895fbd5f64e2d57b374582a8
eora21 <eora21@naver.com>
2022-12-12 13:41:41 +0900

    feat: 🎸 추가 기능 선택 출력문

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,9 +1,23 @@
 package view;
 
+import domain.Feature;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
+
+    public static int inputFeature() {
+        System.out.println("## 메인화면");
+
+        for (Feature feature : Feature.values()) {
+            System.out.println(feature);
+        }
+
+        System.out.println();
+        System.out.println("## 원하는 기능을 선택하세요.");
+        return scanner.nextInt();
+    }
 
     public static int inputTableNumber() {
         System.out.println("## 주문할 테이블을 선택하세요.");
