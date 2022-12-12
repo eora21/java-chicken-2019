c2640cf8977d53de3f8d4d7c2d7bfac7d4737c9b
eora21 <eora21@naver.com>
2022-12-12 13:45:18 +0900

    feat: 🎸 추가 숫자로 원하는 기능을 반환하는 메서드

--- src/main/java/domain/Feature.java
+++ src/main/java/domain/Feature.java
@@ -1,5 +1,7 @@
 package domain;
 
+import java.util.Arrays;
+
 public enum Feature {
     ORDER(1, "주문등록"),
     PAYMENT(2, "결제하기"),
@@ -11,6 +13,13 @@ public enum Feature {
     Feature(int number, String description) {
         this.number = number;
         this.description = description;
+    }
+
+    public static Feature findFeatureByNumber(int number) {
+        return Arrays.stream(Feature.values())
+                .filter(feature -> feature.number == number)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("일치하는 기능이 없습니다."));
     }
 
     @Override
