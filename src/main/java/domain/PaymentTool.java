2070456fe145a735251c8b6ab23d4e094e50a757
eora21 <eora21@naver.com>
2022-12-12 17:39:35 +0900

    feat: 🎸 추가 결제 방법

---
+++ src/main/java/domain/PaymentTool.java
@@ -0,0 +1,26 @@
+package domain;
+
+import java.util.Arrays;
+import java.util.function.IntUnaryOperator;
+
+public enum PaymentTool {
+    CARD(1, "신용 카드", total -> total),
+    CASH(2, "현금", total -> (int)(total * 0.95));
+
+    private final int number;
+    private final String description;
+    private final IntUnaryOperator calculate;
+
+    PaymentTool(int number, String description, IntUnaryOperator calculate) {
+        this.number = number;
+        this.description = description;
+        this.calculate = calculate;
+    }
+
+    public static PaymentTool findPaymentToolByNumber(int number) {
+        return Arrays.stream(PaymentTool.values())
+                .filter(paymentTool -> paymentTool.number == number)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("입력값을 다시 확인해주세요"));
+    }
+}
